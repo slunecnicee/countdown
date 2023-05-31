d8644f2fc12c754865f181595a16f767400c12a4
slunecnicee <129013318+slunecnicee@users.noreply.github.com>
2023-05-31 05:21:06 +0400

    countdown

---
+++ countdown/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CountdownTimer from './components/countdown';
+
+function App() {
+  return (
+    <div className="App">
+    <CountdownTimer/>
+    </div>
+  );
+}
+
+export default App;
